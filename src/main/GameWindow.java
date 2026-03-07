52083cba1f2997e99032e1d2007bf16d218f06a1
arup-sarkar-24 <1arupsarkar112@gmail.com>
2026-03-07 11:42:58 +0530

    first commit

---
+++ src/main/GameWindow.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow {
+    private JFrame jFrame;
+
+    public GameWindow(){
+        jFrame = new JFrame();
+        jFrame.setSize(800,600);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jFrame.setVisible(true);
+    }
+}
