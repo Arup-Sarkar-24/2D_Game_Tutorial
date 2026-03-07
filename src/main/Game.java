051728ebf60a73101d2e416cf1d6343984e20d2f
arup-sarkar-24 <1arupsarkar112@gmail.com>
2026-03-07 13:18:25 +0530

    image added

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,14 +1,49 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
 
     private GameWindow gameWindow;
     private GamePanel gamePanel;
+    private Thread gameThread;
+    private final int FPS_SET = 120;
 
     public Game(){
         gamePanel = new GamePanel();
-
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
+        startGameLoop();
+    }
+
+    private void startGameLoop(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        long lastFrame = System.nanoTime();
+        long now ;
+        int frames =0;
+        long lastCkeck=System.currentTimeMillis();
+
+        while (true) {
+            now = System.nanoTime();
+            if (now - lastFrame >= timePerFrame) {
+                gamePanel.repaint();
+                lastFrame = now;
+                frames++;
+
+            }
+            //Game Engine tip
+            //Thread.yield(); // or Thread.sleep(1)
+            if (System.currentTimeMillis()-lastCkeck>=1000){
+                lastCkeck=System.currentTimeMillis();
+                System.out.println("FPS:"+frames);
+                frames=0;
+            }
+        }
+
     }
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,20 +3,44 @@ package main;
 import inputs.KeyBoardInputs;
 import inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {
 
     private MouseInputs mouseInputs;
     private float xDelta=100, yDelta=100;
+    private BufferedImage img, subImg;
+
 
     public GamePanel(){
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyBoardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+        setPanelSize();
+        importImg();
 
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+        try {
+            img = ImageIO.read(is);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(640, 400);
+        setPreferredSize(size);
+        setMinimumSize(size);
+        setMaximumSize(size);
     }
 
     public void changeXDelta(int value){
@@ -34,8 +58,8 @@ public class GamePanel extends JPanel {
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        subImg= img.getSubimage(1*64,8*40,64,40);
 
-        g.fillRect((int) xDelta,(int) yDelta,200,50);
-        repaint();
+        g.drawImage(subImg,(int)xDelta,(int)yDelta,128,80,null);
     }
 }
